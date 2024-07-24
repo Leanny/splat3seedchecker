bb5c7e28bf301308d31d3d2d9ab6b2ac080ea58c
Leanny <tales-of@live.de>
2024-07-24 23:07:45 +0200

    fix stuff for qr code titles

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.4bdab718.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksplat3seedchecker=self.webpackChunksplat3seedchecker||[]).push([[5539],{6406:e=>{function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.4bdab718.chunk.js.map
